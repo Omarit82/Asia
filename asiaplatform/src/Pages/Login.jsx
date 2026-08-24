588526ccf8d98061baa49fd62aba7d809c8277d5
Omar Roselli <roselliomar82@gmail.com>
2026-08-23 21:52:49 -0300

    FeatureLogin: CSS applied

--- asiaplatform/src/Pages/Login.jsx
+++ asiaplatform/src/Pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
 import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import "../assets/css/login.css";
 
 export function Login() {
 
@@ -37,24 +39,63 @@ export function Login() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <div className="d-flex golden-background">
+            <div className=" text-center logo d-flex flex-column justify-content-center align-items-center">
+                <img src="./public/images/Asia.png" alt="Logo Asia" />
+                <h2>Plataforma Asia</h2>
+            </div>
+            <div className="container">
+                <div className="row justify-content-center align-items-center vh-100 me-5">
+                    <div className="">
+                        <div className="card shadow">
 
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
+                            <div className="card-body ">
 
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
+                                <h2 className="text-center mb-4">
+                                    Iniciar Sesión
+                                </h2>
 
-            <button type="submit">
-                Ingresar
-            </button>
+                                <form onSubmit={handleSubmit}>
 
-        </form>
+                                    <div className="mb-3">
+                                        <label className="form-label">
+                                            Email
+                                        </label>
+
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            placeholder="correo@ejemplo.com"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <label className="form-label">
+                                            Contraseña
+                                        </label>
+
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            placeholder="Contraseña"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <button className="btn btn-login w-100" type="button"><FcGoogle size={20} className="me-2" />Continuar con Google</button>
+                                    </div>
+                                    <button type="submit" className="btn btn-ingresar w-100">Ingresar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
